d36a41e4f008d7982644996eb04ee3a75c2be310
ivanwada <ivanwada_zivy@hotmail.com>
2015-05-17 16:19:38 +0200

    Añadida la posibilidad de elegir usuario y cantidad de tweets a jsontwitter

--- jsontwitter.py
+++ jsontwitter.py
@@ -8,7 +8,10 @@ ACCESS_KEY = "3092945518-BWFv6uyTxOGBMM6v2k77FTDER5cUiEMWsf41jv1"
 ACCESS_SECRET = "hiRBJPrSLsSL9LRQD6XKkBobotwfTbsLH2nGTa3EdEawD"
 auth = twitter.OAuth(ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 t = twitter.Twitter(auth=auth)
-datos = t.statuses.user_timeline(user_id=107793431, count=6)
+usuario = raw_input("Nombre de usuario deseado: ")
+numero = raw_input("Cantidad de tweets: ")
+userid = t.users.show(screen_name=usuario)['id'] #Consigue la id del usuario que elegimos
+datos = t.statuses.user_timeline(user_id=userid, count=numero) #Escoge los tweets del usuario elegido y la cantidad de tweets
 
 for tweet in datos:
-	print tweet['text']
+	print tweet['text'] 
