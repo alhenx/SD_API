6cf498bd450a338ff7a8403ebe155f8f23f5ed3a
Alhen11 <alhenx@gmail.com>
2015-05-18 12:16:15 +0200

    Servicios adaptados

--- api.py
+++ api.py
@@ -6,9 +6,9 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from werkzeug import secure_filename
 import tweepy
 import twitter
-import time
+import pytumblr
 
-
+#Autentificación de Tweepy
 def oauth_login():
     CONSUMER_KEY = 'FrmCRULEy54WAJyEEzslKBlQf'
     CONSUMER_SECRET = 'TaFwRAtW96yYxfHG54ct4VWyZ3re7tzm8odAJziHlewBsjqGcS'
@@ -24,16 +24,19 @@ def oauth_login():
     twit = tweepy.API(auth)
     return twit
 
+#Twittear estado nuevo junto a imagen
 def twittear_estado_img(estado):
     twit = oauth_login() 
     status = estado
     fn = os.path.abspath('uploads/foto.jpg')
     twit.update_with_media(fn, status=status)
 
+#Twittear estado nuevo
 def twittear_estado(estado):
     twit = oauth_login() 
     twit.update_status(status=estado)
 
+#Ver los dos últimos twits de un usuario
 def ver_estado(nombre,num):
     CONSUMER_KEY = 'FrmCRULEy54WAJyEEzslKBlQf'
     CONSUMER_SECRET = 'TaFwRAtW96yYxfHG54ct4VWyZ3re7tzm8odAJziHlewBsjqGcS'
@@ -41,8 +44,8 @@ def ver_estado(nombre,num):
     ACCESS_SECRET = 'WIzKDbwqZsKJrxtzI9RwCFAhP7WP85etO4mOZDpL29Aji'
     auth = twitter.OAuth(ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
     t = twitter.Twitter(auth=auth)
-    userid = t.users.show(screen_name=nombre)['id'] #Consigue la id del usuario que elegimos
-    datos = t.statuses.user_timeline(user_id=userid, count=num) #Escoge los tweets del usuario elegido y la cantidad de tweets
+    userid = t.users.show(screen_name=nombre)['id']
+    datos = t.statuses.user_timeline(user_id=userid, count=num)
 
     f=open("static/txt/tuits.txt","w")
     for tweet in datos:
@@ -50,17 +53,26 @@ def ver_estado(nombre,num):
         f.write(tuit+'\n') 
     f.close()
 
-'''
-#TWITTER + DRIVE
-def twitter_drive():
+    
 
-#DRIVE
-def subir_drive():
+#Sube una foto a tumblr
+def subir_tumblr():
+    consumer_key = '6XuUsNVBhpF3I2gkXMTq8jkmoTZYQvp16LsHGCFGBV7X5ObOQW'
+    consumer_secret = 'FpChhkP29A8tTxlYgQCCakWwO6Ibx2HX8CmiRj8dyHOFWL5efN'
+    token_key = 'FlrqWswNK29GrHAisl5RuE3xRU0tZBHJMmG8r6DUzcWNGS0OxL' 
+    token_secret = 'uGKcyOIQ2BrcgFDLsAv6sBrWoesO6ZU63mmR6MTPJXM5tom8qx'
+
+    client = pytumblr.TumblrRestClient(
+        consumer_key,
+        consumer_secret,
+        token_key,
+        token_secret
+    )
+    fn = os.path.abspath('uploads/foto.jpg')
+    client.create_photo('sd2015blog', state="published", tags=["SD"], data=fn)
 
 
-'''
-
-# Initialize the Flask application
+#Inicia flask
 app = Flask(__name__)
 
 # Carpeta donde se guarda la imagen temporalmente
@@ -74,6 +86,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+#Index
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -82,30 +95,37 @@ def index():
 def index_inicio():
     return render_template('index.html')
 
+#Página para subir foto a twitter
 @app.route('/twitterimg')
 def index_img():
     return render_template('twitterimg.html')
 
+#Página para twittear estado
 @app.route('/twittersta')
 def index_sta():
     return render_template('twittersta.html')
 
-@app.route('/twitterdrive')
-def index_twdrive():
-    return render_template('twitterdrive.html')
+#Página para subir foto a tumblr y twitter
+@app.route('/twittertumblr')
+def index_twtumblr():
+    return render_template('twittertumblr.html')
 
-@app.route('/drive')
-def index_drive():
-    return render_template('drive.html')
+#Página para subir foto a tumblr
+@app.route('/tumblr')
+def index_tumblr():
+    return render_template('tumblr.html')
 
+#Página para introducir el nombre de quien se desea ver el estado
 @app.route('/jsontw')
 def index_jsontw():
     return render_template('jsontw.html')
 
+#Página donde se ven los twits
 @app.route('/vertw')
 def vertw():  
     return render_template("vertw.html")
 
+#Subir foto a twitter y estado
 @app.route('/uploadimg', methods=['POST'])
 def upload_img():
     file = request.files['file']
@@ -116,12 +136,14 @@ def upload_img():
         twittear_estado_img(estado)
         return index()
 
+#Actualizar estado en twitter
 @app.route('/uploadsta', methods=['POST'])
 def upload_sta():
     estado = request.form['estado']
     twittear_estado(estado)
     return index()
 
+#Ver últimos twits
 @app.route('/versta', methods=['POST'])
 def ver_sta():
     nombre = request.form['nombre']
@@ -129,19 +151,29 @@ def ver_sta():
     ver_estado(nombre,2)
     #return index_jsontw()
     return vertw()
-'''
-#TWITTER + DRIVE
-@app.route('/uploadtwdrive', methods=['POST'])
-def upload_twdrive():
-    twittear_drive();
-    return index()
 
-#DRIVE
-@app.route('/uploaddrive', methods=['POST'])
-def upload_drive():
-    subir_drive();
-    return index()
-'''
+#Subir foto a twitter y tumblr
+@app.route('/uploadtwtumblr', methods=['POST'])
+def upload_twtumblr():
+    file = request.files['file']
+    estado = request.form['estado']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'foto.jpg'))
+        twittear_estado_img(estado)
+        subir_tumblr()
+        return index()
+
+#Subir foto a tumblr
+@app.route('/uploadtumblr', methods=['POST'])
+def upload_tumblr():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'foto.jpg'))
+        subir_tumblr()
+        return index()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
